6467cd09cbc7578ea7f24dee344e60b9db884b98
youssef elhoubi <elhoubiyoussef@gmail.com>
2025-01-09 22:58:57 +0100

    ceated the add blog end pint

---
+++ controllers/blogs/addBlog.php
@@ -0,0 +1,14 @@
+<?php
+include "../../Class/blogs/blog.php";
+include "../../instance/instace.php";
+
+$blog = new Blog($pdo);
+$result = $blog->addBlog();
+if ($result['status']==1) {
+    header("location: ../../client/blogs/index.php");
+}else{
+    echo $result['message'];
+    exit;
+}
+
+?>
