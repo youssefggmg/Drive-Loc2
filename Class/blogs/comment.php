141c340555f8ad8663c5d1b22d769ad3fa11d0e4
youssef elhoubi <elhoubiyoussef@gmail.com>
2025-01-09 15:40:08 +0100

    created a class for the comments and made the create commant method

---
+++ Class/blogs/comment.php
@@ -0,0 +1,38 @@
+<?php
+class Comment
+{
+    private $dbcon;
+    private $id;
+    private $text;
+    private $blogID;
+    private $UserID;
+    public function __construct($dbcon)
+    {
+        $this->dbcon = $dbcon;
+    }
+    public function createComment()
+    {
+        $this->blogID = $_POST["BLOGID"];
+        $this->UserID = $_POST["USERID"];
+        $this->text = $_POST["COMMENT"];
+        $query = "INSERT INTO comment(centext, userID, blogID) VALUES (':text', ':user', ':blog')";
+        try {
+            $stmt = $this->dbcon->prepare($query);
+            $executed = $stmt->execute([
+                'text' => $this->text,
+                'user' => $this->UserID,
+                'blog' => $this->blogID
+            ]);
+            if ($executed) {
+                return ["status" => 1, "message" => "comment was added sucssesfully"];
+            } else {
+                return ["status" => 0, "message" => "comment was not added"];
+            }
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        }
+    }
+    
+
+}
+?>
